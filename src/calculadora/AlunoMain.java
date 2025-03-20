9de5f01d043b25e1bf3403860756961ca576e3b6
unknown <kaua080104>
2025-03-19 22:12:40 -0300

    atividadePronta

---
+++ src/calculadora/AlunoMain.java
@@ -0,0 +1,12 @@
+package calculadora;
+
+public class Calculadora {
+
+    public static void main(String[] args) {
+        // Crie a instância da janela de diálogo
+        NewJDialog dialog = new NewJDialog(new javax.swing.JFrame(), true);
+        
+        // Torne o diálogo visível
+        dialog.setVisible(true);
+    }
+}
